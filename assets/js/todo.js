b08ea8cd3c8491d82a80258da8d338f44f7e1e05
Michael Hann <michaelhann@MacBook-Pro.local>
2020-10-22 20:31:50 +0100

    Updated for single todo list format

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,263 +1,186 @@
-const listsContainer = document.querySelector(".task-lists");
-const listInput = document.querySelector("#list-input");
-const listBtn = document.querySelector("#addList");
+//.....TODO LIST.....
 
-const LOCAL_STORAGE_LIST_KEY = "task.storedLists";
-const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId";
+//..........SELECTORS..........
+const input = document.querySelector("#input");
+const todoBtn = document.querySelector("#addItem");
+const list = document.querySelector("#list");
+const edit = document.querySelector(".edit");
+const reset = document.querySelector(".reset");
+const deleteC = document.querySelector(".delete-completed");
 
-// DISPLAY STORED TASK LISTS (IF ANY) WHEN PAGE LOADS
-document.addEventListener("DOMContentLoaded", getLists);
+const check = "fa-check-circle";
+const uncheck = "fa-circle";
+const linethrough = "completed linethrough";
 
-function getLists () {
+
+// RENDER STORED TASK ITEMS (IF ANY) WHEN PAGE LOADS
+document.addEventListener("DOMContentLoaded", getToDo);
+
+function getToDo () {
   // Check if tasks already stored
-  if(localStorage.getItem(LOCAL_STORAGE_LIST_KEY) === null) {
+  if(localStorage.getItem("storedList") === null) {
     return;
   }
-  // Recall items from local storage
-  storedLists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY));
-  storedLists.forEach(function(item) {
-      displayList(item.title, item.id, item.tasks);
+
+  let storedList;
+  //Recall items from local storage
+  storedList = JSON.parse(localStorage.getItem("storedList"));
+  storedList.forEach(function(item) {
+      renderItem(item.todo, item.completed);
   });
 }
 
-// ADD TODO
+// ADD TASK
 // (a) If input contains value and PLUS button pressed
-listBtn.addEventListener("click", function() {
-    let list = listInput.value;
-    if(list) {
-        let uniqueId = Date.now().toString();
-        addList(list, uniqueId);
+todoBtn.addEventListener("click", function() {
+    let todo = input.value;
+    if(todo) {
+        addItem(todo);
     }
 });
 
 // (b) If input contains value and return key (13) pressed
 document.addEventListener("keypress", function(event) {
     if(event.which === 13) {
-        let list = listInput.value;
-        if(list) {
-            let uniqueId = Date.now().toString();
-            addList(list, uniqueId);
-            
+        let todo = input.value;
+        if(todo) {
+            addItem(todo);
         }
     }
 });
 
-function addList(list, uniqueId) {
-  displayList(list, uniqueId);
-  listInput.value="";
+function addItem(todo) {
+  renderItem(todo);
+  input.value="";
 }
 
-// DISPLAY TODO
-function displayList(list, uniqueId) {
-
-  const html = `<li class="task-list" id="${uniqueId}">
+// RENDER TASK
+function renderItem(todo, completed) {
+  const done = completed ? check : uncheck;
+  const line = completed ? linethrough : "";
+  
+  const item = `<li>
+                    <span contenteditable="false" class="text ${line}">${todo}</span>
+                    <i class="check far ${done}"></i>
                     <i class="trash fas fa-trash-alt"></i>
                     <i class="edit far fa-edit"></i>
-                    <span contenteditable="false" class="text">${list}</span>
                     <i class="save far fa-save"></i>
                 </li>`
 
   const position = "beforeend";
-  listsContainer.insertAdjacentHTML(position, html);
-
-  // if (list.id === selectedListId) {
-  //   item.classList.add("active-list");
-  // }
-
-  saveList();
+  list.insertAdjacentHTML(position, item);
+  reset.classList.add("display-btn");
+  deleteC.classList.add("display-btn");
+  saveToDo();
 }
 
-
-listsContainer.addEventListener("click", selectTaskList);
-
-function selectTaskList(event) {
-  let element = event.target;
-  if(element.tagName.toLowerCase() === "li") {
-    selectedListId = element.id;
-    console.log(selectedListId);
-  };
-}
-
-// SAVE TASK TO LOCAL STORAGE
-function saveList (list) {
-  const taskItems = document.querySelectorAll("li");
+// SAVE TASKS TO LOCAL STORAGE
+function saveToDo (todo) {
+  const todoItems = document.querySelectorAll("li");
   const item = document.querySelector("li");
-  const id = document.querySelector("li").id;
-  const storedLists = [];
 
-  taskItems.forEach(function(item) {
-    storedLists.push({
-      id: item.id,
-      title: item.querySelector("span").innerText,
-      tasks: []
+  const storedList = [];
+
+  todoItems.forEach(function(item) {
+    storedList.push({
+      todo: item.innerText,
+      completed: item.querySelector("span").classList.contains("completed"),
     });
   });
-  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(storedLists));
+  localStorage.setItem("storedList", JSON.stringify(storedList));
+}
+
+// DELETE TASK
+list.addEventListener("click", deleteToDo);
+
+function deleteToDo(event) {
+    let element = event.target;
+    const storedList = getToDo;
+    if(element.classList.contains("trash")) {
+      element.parentElement.remove();
+      saveToDo();
+    }
+}
+
+// EDIT TASK
+list.addEventListener("click", editTodo);
+
+function editTodo(event) {
+    let element = event.target;
+    let span = element.parentNode.querySelector(".text");
+    let save = element.parentNode.querySelector(".save");
+    let check = element.parentNode.querySelector(".check");
+    let edit = element.parentNode.querySelector(".edit");
+    let trash = element.parentNode.querySelector(".trash");
+
+    if(element.classList.contains("edit")) {
+        span.contentEditable = true;
+        save.classList.add("display-save");
+        check.classList.add("noDisplay");
+        edit.classList.add("noDisplay");
+        trash.classList.add("noDisplay");
+        span.focus();
+
+        span.addEventListener("keypress", function(event) {
+            if(event.which === 13) {
+                span.contentEditable = false;
+                save.classList.remove("display-save");
+                check.classList.remove("noDisplay");
+                edit.classList.remove("noDisplay");
+                trash.classList.remove("noDisplay");
+            }
+            saveToDo();
+        });
+
+        save.addEventListener("click", function(event) {
+            let element = event.target;
+
+            if(element.classList.contains("save")) {
+                span.contentEditable = false;
+                save.classList.remove("display-save");
+                check.classList.remove("noDisplay");
+                edit.classList.remove("noDisplay");
+                trash.classList.remove("noDisplay");
+            }
+            saveToDo();
+        });
+    }
+}
+
+// COMPLETE TASK
+list.addEventListener("click", completeToDo);
+
+function completeToDo(event) {
+  let element = event.target;
+  if(element.classList.contains("check")) {
+    element.classList.toggle("fa-circle");
+    element.classList.toggle("fa-check-circle");
+    element.parentNode.querySelector(".text").classList.toggle("completed");
+    element.parentNode.querySelector(".text").classList.toggle("linethrough");
+    saveToDo();
+  }
+}
+
+// DELETE COMPLETED TASKS
+deleteC.addEventListener("click", deleteCompleted);
+
+function deleteCompleted() {
+  let storedList = JSON.parse(localStorage.getItem("storedList"));
+  storedList = storedList.filter(todo => !todo.completed);
+  console.log(storedList);
+  localStorage.setItem("storedList", JSON.stringify(storedList));
+  renderItem(storedList);
 }
 
 
 
-// //.....TODO LIST.....
+// DELETE ALL TASKS
+reset.addEventListener("click", deleteAll);
 
-// //..........SELECTORS..........
-// const input = document.querySelector("#input");
-// const todoBtn = document.querySelector("#addItem");
-// const listItems = document.querySelector("#todo-items");
-// const edit = document.querySelector(".edit");
-// const reset = document.querySelector(".reset");
-
-// const check = "fa-check-circle";
-// const uncheck = "fa-circle";
-// const linethrough = "completed linethrough";
-
-
-// // DISPLAY STORED TODO ITEMS (IF ANY) WHEN PAGE LOADS
-// document.addEventListener("DOMContentLoaded", getToDo);
-
-// function getToDo () {
-//   // Check if tasks already stored
-//   if(localStorage.getItem("storedList") === null) {
-//     return;
-//   }
-
-//   let storedList;
-//   //Recall items from local storage
-//   storedList = JSON.parse(localStorage.getItem("storedList"));
-//   storedList.forEach(function(item) {
-//       displayItem(item.todo, item.completed);
-//   });
-// }
-
-// // ADD TODO
-// // (a) If input contains value and PLUS button pressed
-// todoBtn.addEventListener("click", function() {
-//     let todo = input.value;
-//     if(todo) {
-//         addItem(todo);
-//     }
-// });
-
-// // (b) If input contains value and return key (13) pressed
-// document.addEventListener("keypress", function(event) {
-//     if(event.which === 13) {
-//         let todo = input.value;
-//         if(todo) {
-//             addItem(todo);
-//         }
-//     }
-// });
-
-// function addItem(todo) {
-//   displayItem(todo);
-//   input.value="";
-// }
-
-// // DISPLAY TODO
-// function displayItem(todo, completed) {
-//   const done = completed ? check : uncheck;
-//   const line = completed ? linethrough : "";
-  
-//   const item = `<li id="todo-item">
-//                     <i class="trash fas fa-trash-alt"></i>
-//                     <i class="edit far fa-edit"></i>
-//                     <span contenteditable="false" class="text ${line}">${todo}</span>
-//                     <i class="save far fa-save"></i>
-//                     <i class="check far ${done}"></i>
-//                 </li>`
-
-//   const position = "beforeend";
-//   listItems.insertAdjacentHTML(position, item);
-//   reset.classList.add("display-btn");
-//   saveToDo();
-// }
-
-// // SAVE TODO TO LOCAL STORAGE
-// function saveToDo (todo) {
-//   const todoItems = document.querySelectorAll("#todo-item");
-//   const item = document.querySelector("#todo-item");
-
-//   const storedList = [];
-
-//   todoItems.forEach(function(item) {
-//     storedList.push({
-//       // id: Date.now().toString(),
-//       todo: item.innerText,
-//       completed: item.querySelector("span").classList.contains("completed")
-//     });
-//   });
-//   localStorage.setItem("storedList", JSON.stringify(storedList));
-// }
-
-// // DELETE TODO
-// listItems.addEventListener("click", deleteToDo);
-
-// function deleteToDo(event) {
-//     let element = event.target;
-//     const storedList = getToDo;
-//     if(element.classList.contains("trash")) {
-//       element.parentElement.remove();
-//       saveToDo();
-//     }
-// }
-
-// // EDIT TODO
-// listItems.addEventListener("click", editTodo);
-
-// function editTodo(event) {
-//     let element = event.target;
-//     let span = element.parentNode.querySelector(".text");
-//     let save = element.parentNode.querySelector(".save");
-//     let check = element.parentNode.querySelector(".check");
-
-//     if(element.classList.contains("edit")) {
-//         span.contentEditable = true;
-//         save.classList.add("display-save");
-//         check.classList.add("noDisplay");
-//         span.focus();
-
-//         span.addEventListener("keypress", function(event) {
-//             if(event.which === 13) {
-//                 span.contentEditable = false;
-//                 save.classList.remove("display-save");
-//                 check.classList.remove("noDisplay");
-//             }
-//             saveToDo();
-//         });
-
-//         save.addEventListener("click", function(event) {
-//             let element = event.target;
-
-//             if(element.classList.contains("save")) {
-//                 span.contentEditable = false;
-//                 save.classList.remove("display-save");
-//                 check.classList.remove("noDisplay");
-//             }
-//             saveToDo();
-//         });
-//     }
-// }
-
-// // COMPLETE TODO
-// listItems.addEventListener("click", completeToDo);
-
-// function completeToDo(event) {
-//   let element = event.target;
-//   if(element.classList.contains("check")) {
-//     element.classList.toggle("fa-circle");
-//     element.classList.toggle("fa-check-circle");
-//     element.parentNode.querySelector(".text").classList.toggle("completed");
-//     element.parentNode.querySelector(".text").classList.toggle("linethrough");
-//     saveToDo();
-//   }
-// }
-
-// // DELETE ALL TODOS
-// reset.addEventListener("click", deleteAll);
-
-// function deleteAll() {
-//   var question = confirm("Delete your entire ToDo list?");
-//   if(question) {
-//     localStorage.clear();
-//     location.reload();
-//   }
-// }
+function deleteAll() {
+  var question = confirm("Delete your entire ToDo list?");
+  if(question) {
+    localStorage.clear();
+    location.reload();
+  }
+}
